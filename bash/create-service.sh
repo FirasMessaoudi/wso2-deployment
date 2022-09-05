41db519a07506fc95ddb3045badfcee1dfad5951
Firas Messaoudi <firas_messaoudi@outlook.fr>
2022-09-05 15:51:28 +0100

    Update create-service.sh

--- bash/create-service.sh
+++ bash/create-service.sh
@@ -1,1 +1,6 @@
 
+KUBECTL=`which kubectl`
+
+${KUBECTL} apply -f ../services/elasticsearch-deployment.yaml -n wso2
+${KUBECTL} apply -f ../services/wso2apim-deployment.yaml -n wso2
+${KUBECTL} apply -f ../services/kibana-deployment.yaml -n wso2
